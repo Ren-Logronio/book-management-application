2d47198ade997186e1b3b736a9935cbc1c07bd8c
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-12-06 21:21:55 +0800

    added routing

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function LoginPage(){
+    return (
+        <div>
+            <Link to="/">Back To Index</Link>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
